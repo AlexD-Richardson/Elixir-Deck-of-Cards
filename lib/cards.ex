9c547d914262eed33eb47cd384e753fb582f3679
Alex Richardson <richardson.d.alex@gmail.com>
2019-01-14 10:49:31 -0500

    Finished methods to create, shuffle, save, and load the deck

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,42 @@
 defmodule Cards do
   @moduledoc """
-  Documentation for Cards.
+  Creates a deck of cards and allows users to shuffle the deck.
   """
 
   @doc """
-  Hello world.
+
 
   ## Examples
 
-      iex> Cards.hello()
-      :world
-
   """
-  def hello do
-    :world
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "King", "Queen"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    {status, binary} = File.read(filename)
+
+    case status do
+      :ok -> :erlang.binary_to_term binary
+      :error -> "That file does not exist"
+    end
   end
 end
