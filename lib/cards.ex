24a5b6e2d810c2c944b22d072f7d52b5971fe038
Alex Richardson <richardson.d.alex@gmail.com>
2019-01-16 09:49:10 -0500

    Added more documentation for methods

--- lib/cards.ex
+++ lib/cards.ex
@@ -18,19 +18,39 @@ defmodule Cards do
     end
   end
 
+  @doc """
+
+  Allows user to shuffle around the elements inside the deck using the Enum module
+
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  @doc """
+
+  Allows user to deal cards out and they can enter a specific deck out and even specify the hand size
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
 
+  @doc """
+
+  Allows a user to save a specific deck, this will create a file for the specified deck that could let the user load it up again if need be
+
+  """
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
 
+  @doc """
+
+  Allows user to load up the deck they had previously saved to a file for use again.
+
+  """
   def load(filename) do
     {status, binary} = File.read(filename)
 
